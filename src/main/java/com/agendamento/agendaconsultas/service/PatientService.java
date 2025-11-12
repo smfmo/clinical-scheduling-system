b53a2fa9f6b702ce46c783b77d3f0c4a6d3fa9f1
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-11-11 21:44:52 -0300

    feat (service): Implementar métodos CRUD para a entidade Patient

--- src/main/java/com/agendamento/agendaconsultas/service/PatientService.java
+++ src/main/java/com/agendamento/agendaconsultas/service/PatientService.java
@@ -4,6 +4,8 @@ import com.agendamento.agendaconsultas.model.Patient;
 import com.agendamento.agendaconsultas.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -15,7 +17,19 @@ public class PatientService {
     }
 
     @Transactional
-    public void create(Patient patient) {
+    public void registerPatient(Patient patient) {
         repository.save(patient);
+    }
+
+    public List<Patient> findAll() {
+        return repository.findAll();
+    }
+
+    public Patient findById(UUID patientId) {
+        return repository.findById(patientId).orElse(null);
+    }
+
+    public void deletePatient(UUID patientId) {
+        repository.deleteById(patientId);
     }
 }
