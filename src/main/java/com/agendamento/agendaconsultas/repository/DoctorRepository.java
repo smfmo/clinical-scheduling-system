1886e1348b1c2063a494b6f6fb19f84460cf5e4f
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-11-11 21:43:25 -0300

    feat (repository): Implementar query para buscar médicos pela especialidade

--- src/main/java/com/agendamento/agendaconsultas/repository/DoctorRepository.java
+++ src/main/java/com/agendamento/agendaconsultas/repository/DoctorRepository.java
@@ -1,10 +1,16 @@
 package com.agendamento.agendaconsultas.repository;
 
 import com.agendamento.agendaconsultas.model.Doctor;
+import com.agendamento.agendaconsultas.model.enums.Specialty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
+
+    @Query("SELECT d FROM Doctor d WHERE d.specialty = :speciality")
+    List<Doctor> searchBySpeciality(Specialty specialty);
 }
